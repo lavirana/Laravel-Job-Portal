4650e46b0b44d0fc955806cf645a377505246877
Ashish Rana <ashishrana288@gmail.com>
2025-05-06 00:08:55 +0530

    added user profile page code

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -79,6 +79,39 @@ class AccountController extends Controller
     }
 
 
+    public function updateProfile(Request $request){
+        $id = Auth::user()->id;
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|min:5|max:20',
+            'email' => 'required|email|unique:users,email,'.$id.',id',
+]);
+
+
+
+                    if($validator->passes()){
+
+                        $user = User::find($id);
+                        $user->name = $request->name;
+                        $user->email = $request->email;
+                        $user->mobile = $request->mobile;
+                        $user->designation = $request->designation;
+                        $user->save();
+
+                        session()->flash('success','Profile Updated successfully.');
+
+                        return response()->json([
+                            'status' => true,
+                            'errors' => []
+                        ]);
+
+                    } else{
+                        return response()->json([
+                            'status' => false,
+                            'errors' => $validator->errors()
+                        ]);
+                    }
+    }
+
     public function logout(){
         Auth::logout();
         return redirect()->route('account.login');
