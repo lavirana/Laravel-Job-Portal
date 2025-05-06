0e27861c018b0f7dc524e6ca3acf8baef7dc6f17
Ashish Rana <ashishrana288@gmail.com>
2025-05-06 15:06:37 +0530

    added user profile page code

--- resources/views/front/account/sidebar.blade.php
+++ resources/views/front/account/sidebar.blade.php
@@ -2,7 +2,7 @@
                     <div class="s-body text-center mt-3">
 
                     @if(Auth::user()->image != '')
-                    <img src="{{ asset('profile_pic/'.Auth::user()->image) }}" alt="avatar"  class="rounded-circle img-fluid" style="width: 150px;">
+                    <img src="{{ asset('profile_pic/thumb/'.Auth::user()->image) }}" alt="avatar"  class="rounded-circle img-fluid" style="width: 150px;">
                     @else
                     <img src="{{ asset('assets/images/avatar7.png') }}" alt="avatar"  class="rounded-circle img-fluid" style="width: 150px;">
                     @endif
