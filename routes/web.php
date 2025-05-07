3d41088fc6e1cb3a8b9fe4f5d2a555aa2afd7f7c
Ashish Rana <ashishrana288@gmail.com>
2025-05-07 18:53:14 +0530

    added job delete  page code

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::group(['prefix' => 'account'], function(){
 
         Route::get('/my-jobs/edit/{jobId}', [AccountController::class, 'editJob'])->name('account.editJob');
         Route::post('/update-job/{jobId}', [AccountController::class, 'updateJob'])->name('account.updateJob');
+        Route::post('/delete-job', [AccountController::class, 'deleteJob'])->name('account.deleteJob');
         
     });
 });
