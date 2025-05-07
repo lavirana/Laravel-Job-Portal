3275988a361c961dd1297a95c6d8d2be8a89ff33
Ashish Rana <ashishrana288@gmail.com>
2025-05-08 00:40:33 +0530

    added home  page code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Job;
+use App\Models\JobType;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('front.home ');
+
+        $categories = Category::where('status',1)->orderBy('name','ASC')->take(8)->get();
+    $featuredJobs = Job::where('status',1)->orderBy('created_at','DESC')->with('JobType')->where('isFeatured', 1)->take(6)->get();
+
+    $latestJobs = Job::where('status',1)->orderBy('created_at','DESC')->with('jobType')->take(6)->get();
+
+        return view('front.home',[
+          'categories' => $categories,
+         'featuredJobs' => $featuredJobs,
+          'latestJobs' => $latestJobs]);
     }
 
     public function contact(){
